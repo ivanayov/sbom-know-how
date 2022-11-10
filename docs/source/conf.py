7d9e1860b96005e37c2324facf7a27b6972ced41
Ivana Atanasova <iyovcheva@vmware.com>
2022-11-10 17:12:20 +0200

    Update to 'sphinx_rtd_theme' theme
    Signed-off-by: Ivana Atanasova <iyovcheva@vmware.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SBoM Know How'
-copyright = '2022, Ivana Atanasova'
+copyright = '2022, VMware Inc.'
 author = 'Ivana Atanasova'
 release = '0.1'
 
@@ -24,5 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+# html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
